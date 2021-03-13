2d5af842e26d5aa264a0c7c00b836e6ef97759c7
Adit Alware <alwareadit@gmail.com>
2021-03-13 17:12:54 +0530

    Hands in With twilio

--- onboard/src/components/UserEnterComponent.jsx
+++ onboard/src/components/UserEnterComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/style.css';
 import Axios from 'axios';
+import Video from 'twilio-video';
 
 let connected = false;
 let room;
@@ -73,6 +74,182 @@ async function assignMentor(event){
 function assignStudent(event){
 
 }
+
+
+async function connect(username) {
+    let url="https://60c6eeef357e.ngrok.io/login";
+   
+    const usernameInput =await document.getElementById('username');
+    const localDiv =await document.getElementById('local');
+
+    let promise = new Promise((resolve, reject) => {
+        // get a token from the back end
+        let data={username:usernameInput.value};
+         GetRequestApi(url,data).then(_data => {
+            // join video call
+            data = _data;
+            return Video.connect(String(data.token));
+        }).then(_room => {
+            room = _room;
+            if(username != now_streaming){
+                localDiv.className = "participantHidden";
+            }
+            if(username == now_streaming){
+                localDiv.className = "participant";
+            }
+            room.participants.forEach(participantConnected);
+            room.on('participantConnected', participantConnected);
+            room.on('participantDisconnected', participantDisconnected);
+            connected = true;
+            updateParticipantCount();
+            devicesDisplay();
+            connectChat(data.token, data.conversation_sid);
+            resolve();
+        }).catch(e => {
+            console.log(e);
+            reject();
+        });
+    });
+    return promise;
+};
+
+
+async function attachTracks(tracks) {
+    const container =await document.getElementById('container');
+
+  tracks.forEach(function(track) {
+    if (track) {
+      let v = track.attach();
+      v.setAttribute('id','video');
+      console.log(v);
+      container.appendChild(v);
+      let d = document.getElementById("myVideo");
+      console.log(d.childNodes);
+      d.removeChild(d.childNodes[1]);
+    }
+  });
+  // console.log("Hrll");
+   
+}
+
+function detachTracks(tracks) {
+  tracks.forEach(function(track) {
+    if (track) {
+      track.detach().forEach(function(detachedElement) {
+        detachedElement.remove();
+      });
+    }
+  });
+}
+
+
+function stopTracks(tracks) {
+  tracks.forEach(function(track) {
+    if (track) { track.stop(); }
+  })
+}
+
+
+
+async function devicesDisplay(){
+  navigator.mediaDevices.enumerateDevices().then(gotDevices);
+  const select = await document.getElementById('video-devices');
+  select.addEventListener('change', updateVideoDevice);
+
+}
+
+async function gotDevices(mediaDevices) {
+  console.log("KK");
+  const select = await document.getElementById('video-devices');
+//   select.innerHTML = `<option value="0">
+//                Select Camera
+//                 </option>`;
+  
+  let count = 1;
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
+}
+
+
+function updateVideoDevice(event) {
+  const select = event.target;
+  const localParticipant = room.localParticipant;
+  if (select.value !== '') {
+    const tracks = Array.from(localParticipant.videoTracks.values()).map(
+      function(trackPublication) {
+        return trackPublication.track;
+      }
+    );
+    localParticipant.unpublishTracks(tracks);
+    console.log(localParticipant.identity + ' removed track: ' + tracks[0].kind);
+
+    detachTracks(tracks);
+    stopTracks(tracks);
+    Video.createLocalVideoTrack({
+      deviceId: { exact: select.value }
+    }).then(function(localVideoTrack) {
+      localParticipant.publishTrack(localVideoTrack);
+      console.log(localParticipant.identity + ' added track: ' + localVideoTrack.kind);
+      const previewContainer = document.getElementById('myVideo');
+      attachTracks([localVideoTrack], previewContainer);
+    });
+  }
+}
+
+
+
+async function updateParticipantCount() {
+    const count = await document.getElementById('count');
+
+    
+    if (!connected)
+        count.innerHTML = 'Disconnected.';
+    else
+        count.innerHTML = (room.participants.size + 1) + ' participants online.';
+};
+
+
+
+async function participantConnected(participant) {
+    const container =await document.getElementById('container');
+
+    let participantDiv = document.createElement('div');
+    participantDiv.setAttribute('id', participant.sid);
+    participantDiv.setAttribute('className',!String(participant.identity).localeCompare(now_streaming)?'participant':'participantHidden');
+    let tracksDiv = document.createElement('div');
+    participantDiv.appendChild(tracksDiv);
+
+    let labelDiv = document.createElement('div');
+    labelDiv.setAttribute('class', 'label');
+    labelDiv.innerHTML = participant.identity;
+    participantDiv.appendChild(labelDiv);
+
+    dict[participant.identity] = participant.sid;
+
+    container.appendChild(participantDiv);
+
+    participant.tracks.forEach(publication => {
+        if (publication.isSubscribed)
+            trackSubscribed(tracksDiv, publication.track);
+    });
+    participant.on('trackSubscribed', track => trackSubscribed(tracksDiv, track));
+    participant.on('trackUnsubscribed', trackUnsubscribed);
+
+    updateParticipantCount();
+};
+
+function participantDisconnected(participant) {
+    document.getElementById(participant.sid).remove();
+    updateParticipantCount();
+};
 
 
 
